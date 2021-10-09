3e5fb1dff0b87218feb07cd9ab49fd14762dbcb9
satyamneo00 <satyam.neo@outlook.com>
2021-10-10 02:42:20 +0530

    Footer CSS changes

--- src/UI/Layout.js
+++ src/UI/Layout.js
@@ -1,12 +1,14 @@
 import Landing from "../Components/Landing"
 import Footer from "./Footer";
 import Header from "./Header"
-
+import styles from '../static/css/Footer.module.css'
 const Layout=props=>{
     return(
         <>
+        <div className={styles.wrapper}>
         <Header/>
         {props.children}
+        </div>
         <Footer/>
         </>
     )
